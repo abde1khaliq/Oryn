3fe982709a20e0201fb609a7323978a7ce957084
abde1khaliq <ahmedthek3@gmail.com>
2026-04-09 11:55:08 +0200

    Add a health check endpoint

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
 
-app = FastAPI(title = 'Oryn', description = """
+app = FastAPI(title='Oryn', description="""
 A backend API where companies sign up, get their own private workspace, 
 and inside it they manage their teams, projects, and tasks — think a simplified Jira or Asana.
 """)
+
+@app.get("/health", tags=["Health"])
+async def health_check():
+    return {
+        "status": "healthy",
+        "app": "Oryn"
+    }
